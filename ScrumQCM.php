9bf57e8ec45eaeb8a45c4e63a27b1cbc27fa8bbf
Matteo <mat.bourbon@hotmail.fr>
2026-03-16 16:16:14 +0100

    Update Questionnaires

--- ScrumQCM.php
+++ ScrumQCM.php
@@ -1,101 +1,84 @@
 <body>
     <section class="questScrum">
-    <form action="x.php" method="POST">
-        <p>Quel est le rôle du Scrum Master dans une équipe ?</p>
-    
-        <input type="radio" id="qr1" name="reponse" value="qr1">
-        <label for="rep1">Définir les objectifs du sprint</label><br>
+        <div class="question">
+            <form action="x.php" method="POST">
+                <p>Quel est le rôle du Scrum Master dans une équipe ?</p>
 
-        <input type="radio" id="qr2" name="reponse" value="qr2">
-        <label for="rep2">Gérer les backlogs du produit</label><br>
+                <input type="radio" id="qr1" name="reponse1" value="qr1">
+                <label for="rep1">Définir les objectifs du sprint</label><br>
 
-        <input type="radio" id="qr3" name="reponse" value="qr3">
-        <label for="rep3">Faciliter le processus Scrum et aider l'équipe à s'améliorer</label><br>
+                <input type="radio" id="qr2" name="reponse1" value="qr2">
+                <label for="rep2">Gérer les backlogs du produit</label><br>
 
-        <input type="radio" id="qr4" name="reponse" value="qr4">
-        <label for="rep4">Prioriser les tâches du sprint</label><br>
+                <input type="radio" id="qr3" name="reponse1" value="qr3">
+                <label for="rep3">Faciliter le processus Scrum et aider l'équipe à s'améliorer</label><br>
 
-        <button type="submit">Valider</button>
-    </form>
-    </section>
+                <input type="radio" id="qr4" name="reponse1" value="qr4">
+                <label for="rep4">Prioriser les tâches du sprint</label><br>
 
-    <section>
-    <form action="x.php" method="POST">
-        <p>Combien de temps dure un sprint Scrum ?</p>
-    
-        <input type="radio" id="qr1" name="reponse" value="qr1">
-        <label for="rep1">1 à 2 semaines</label><br>
 
-        <input type="radio" id="qr2" name="reponse" value="qr2">
-        <label for="rep2">1 à 4 semaines</label><br>
+                <p>Combien de temps dure un sprint Scrum ?</p>
 
-        <input type="radio" id="qr3" name="reponse" value="qr3">
-        <label for="rep3">1 à 3 mois</label><br>
+                <input type="radio" id="qr1" name="reponse2" value="qr5">
+                <label for="rep1">1 à 2 semaines</label><br>
 
-        <input type="radio" id="qr4" name="reponse" value="qr4">
-        <label for="rep4">6 à 12 heures</label><br>
+                <input type="radio" id="qr2" name="reponse2" value="qr6">
+                <label for="rep2">1 à 4 semaines</label><br>
 
-        <button type="submit">Valider</button>
-    </form>
-    </section>
+                <input type="radio" id="qr3" name="reponse2" value="qr7">
+                <label for="rep3">1 à 3 mois</label><br>
 
-    <section>
-    <form action="x.php" method="POST">
-        <p>Quel est la taille recommander dans une équipe Scrum ?</p>
-    
-        <input type="radio" id="qr1" name="reponse" value="qr1">
-        <label for="rep1">de 5 à 9 personnes</label><br>
+                <input type="radio" id="qr4" name="reponse2" value="qr8">
+                <label for="rep4">6 à 12 heures</label><br>
 
-        <input type="radio" id="qr2" name="reponse" value="qr2">
-        <label for="rep2">de 3 à 9 personnes</label><br>
 
-        <input type="radio" id="qr3" name="reponse" value="qr3">
-        <label for="rep3">de 3 à 11 personnes</label><br>
+                <p>Quel est la taille recommander dans une équipe Scrum ?</p>
 
-        <input type="radio" id="qr4" name="reponse" value="qr4">
-        <label for="rep4">de 10 ou moins</label><br>
+                <input type="radio" id="qr1" name="reponse3" value="qr9">
+                <label for="rep1">de 5 à 9 personnes</label><br>
 
-        <button type="submit">Valider</button>
-    </form>
-    </section>
+                <input type="radio" id="qr2" name="reponse3" value="qr10">
+                <label for="rep2">de 3 à 9 personnes</label><br>
 
-    <section>
-    <form action="x.php" method="POST">
-        <p>Qu'est-ce qu'un "User story" ?</p>
-    
-        <input type="radio" id="qr1" name="reponse" value="qr1">
-        <label for="rep1">Un artéfact visuel</label><br>
+                <input type="radio" id="qr3" name="reponse3" value="qr11">
+                <label for="rep3">de 3 à 11 personnes</label><br>
 
-        <input type="radio" id="qr2" name="reponse" value="qr2">
-        <label for="rep2">Une fonctionnalité du produit du point de vue d'un utilisateur</label><br>
+                <input type="radio" id="qr4" name="reponse3" value="qr12">
+                <label for="rep4">de 10 ou moins</label><br>
 
-        <input type="radio" id="qr3" name="reponse" value="qr3">
-        <label for="rep3">Un type de réunion de l'équipe de developpement</label><br>
 
-        <input type="radio" id="qr4" name="reponse" value="qr4">
-        <label for="rep4">Une nouvelle tâche demandé par le Product Owner</label><br>
 
-        <button type="submit">Valider</button>
-    </form>
-    </section>
+                <p>Qu'est-ce qu'un "User story" ?</p>
 
-    <section>
-    <form action="x.php" method="POST">
-        <p>Quels sont les 3 piliers empiriques de Scrum ?</p>
-    
-        <input type="radio" id="qr1" name="reponse" value="qr1">
-        <label for="rep1">Transparence, Inspection, Adaptation</label><br>
+                <input type="radio" id="qr1" name="reponse4" value="qr13">
+                <label for="rep1">Un artéfact visuel</label><br>
 
-        <input type="radio" id="qr2" name="reponse" value="qr2">
-        <label for="rep2">Négotiation, Valeur, Testabilité</label><br>
+                <input type="radio" id="qr2" name="reponse4" value="qr14">
+                <label for="rep2">Une fonctionnalité du produit du point de vue d'un utilisateur</label><br>
 
-        <input type="radio" id="qr3" name="reponse" value="qr3">
-        <label for="rep3">Evaluation, Planification, Execution</label><br>
+                <input type="radio" id="qr3" name="reponse4" value="qr15">
+                <label for="rep3">Un type de réunion de l'équipe de developpement</label><br>
 
-        <input type="radio" id="qr4" name="reponse" value="qr4">
-        <label for="rep4">Carte, Conversation, Confirmation</label><br>
+                <input type="radio" id="qr4" name="reponse4" value="qr16">
+                <label for="rep4">Une nouvelle tâche demandé par le Product Owner</label><br>
 
-        <button type="submit">Valider</button>
-    </form>
+
+                <p>Quels sont les 3 piliers empiriques de Scrum ?</p>
+
+                <input type="radio" id="qr1" name="reponse5" value="qr17">
+                <label for="rep1">Transparence, Inspection, Adaptation</label><br>
+
+                <input type="radio" id="qr2" name="reponse5" value="qr18">
+                <label for="rep2">Négotiation, Valeur, Testabilité</label><br>
+
+                <input type="radio" id="qr3" name="reponse5" value="qr19">
+                <label for="rep3">Evaluation, Planification, Execution</label><br>
+
+                <input type="radio" id="qr4" name="reponse5" value="qr20">
+                <label for="rep4">Carte, Conversation, Confirmation</label><br>
+
+                <button type="submit">Valider</button>
+            </form>
+        </div>
     </section>
 </body>
--- dev-QCM.php
+++ dev-QCM.php
@@ -1,101 +1,81 @@
 <body>
     <section class="questdev">
-    <form action="" method="POST">
-        <p>Qu'est-ce qu'un moteur de recherche ?</p>
-    
-        <input type="radio" id="qr1" name="reponse" value="qr1">
-        <label for="rep1">Un site web mettant en relation avec d'autre</label><br>
+        <form action="" method="POST">
+            <p>Qu'est-ce qu'un moteur de recherche ?</p>
 
-        <input type="radio" id="qr2" name="reponse" value="qr2">
-        <label for="rep2">Un naviguateur Internet</label><br>
+            <input type="radio" id="qr1" name="reponse1" value="qr1">
+            <label for="rep1">Un site web mettant en relation avec d'autre</label><br>
 
-        <input type="radio" id="qr3" name="reponse" value="qr3">
-        <label for="rep3">Une moteur sur lequel rechercher des données et informations</label><br>
+            <input type="radio" id="qr2" name="reponse1" value="qr2">
+            <label for="rep2">Un naviguateur Internet</label><br>
 
-        <input type="radio" id="qr4" name="reponse" value="qr4">
-        <label for="rep4">Une barre de naviguation</label><br>
+            <input type="radio" id="qr3" name="reponse1" value="qr3">
+            <label for="rep3">Une moteur sur lequel rechercher des données et informations</label><br>
 
-        <button type="submit">Valider</button>
-    </form>
-    </section>
+            <input type="radio" id="qr4" name="reponse1" value="qr4">
+            <label for="rep4">Une barre de naviguation</label><br>
 
-    <section>
-    <form action="" method="POST">
-        <p>Quelle fonction retourne la longueur d'une chaîne de texte en PHP ?</p>
-    
-        <input type="radio" id="qr1" name="reponse" value="qr1">
-        <label for="rep1">strlen</label><br>
 
-        <input type="radio" id="qr2" name="reponse" value="qr2">
-        <label for="rep2">strlength</label><br>
+            <p>Quelle fonction retourne la longueur d'une chaîne de texte en PHP ?</p>
 
-        <input type="radio" id="qr3" name="reponse" value="qr3">
-        <label for="rep3">length</label><br>
+            <input type="radio" id="qr1" name="reponse2" value="qr5">
+            <label for="rep1">strlen</label><br>
 
-        <input type="radio" id="qr4" name="reponse" value="qr4">
-        <label for="rep4">substr</label><br>
+            <input type="radio" id="qr2" name="reponse2" value="qr6">
+            <label for="rep2">strlength</label><br>
 
-        <button type="submit">Valider</button>
-    </form>
-    </section>
+            <input type="radio" id="qr3" name="reponse2" value="qr7">
+            <label for="rep3">length</label><br>
 
-    <section>
-    <form action="" method="POST">
-        <p>En JavaScript, lequel de ces types d'évènement n'existe pas ?</p>
-    
-        <input type="radio" id="qr1" name="reponse" value="qr1">
-        <label for="rep1">blur</label><br>
+            <input type="radio" id="qr4" name="reponse2" value="qr8">
+            <label for="rep4">substr</label><br>
 
-        <input type="radio" id="qr2" name="reponse" value="qr2">
-        <label for="rep2">load</label><br>
 
-        <input type="radio" id="qr3" name="reponse" value="qr3">
-        <label for="rep3">mouseclick</label><br>
+            <p>En JavaScript, lequel de ces types d'évènement n'existe pas ?</p>
 
-        <input type="radio" id="qr4" name="reponse" value="qr4">
-        <label for="rep4">mouseout</label><br>
+            <input type="radio" id="qr1" name="reponse3" value="qr9">
+            <label for="rep1">blur</label><br>
 
-        <button type="submit">Valider</button>
-    </form>
-    </section>
+            <input type="radio" id="qr2" name="reponse3" value="qr10">
+            <label for="rep2">load</label><br>
 
-    <section>
-    <form action="" method="POST">
-        <p>Quel attribut permet d'indiquer la destination d'un lien ?</p>
-    
-        <input type="radio" id="qr1" name="reponse" value="qr1">
-        <label for="rep1"> la balise class</label><br>
+            <input type="radio" id="qr3" name="reponse3" value="qr11">
+            <label for="rep3">mouseclick</label><br>
 
-        <input type="radio" id="qr2" name="reponse" value="qr2">
-        <label for="rep2">la balise href</label><br>
+            <input type="radio" id="qr4" name="reponse3" value="qr12">
+            <label for="rep4">mouseout</label><br>
 
-        <input type="radio" id="qr3" name="reponse" value="qr3">
-        <label for="rep3">La balise data</label><br>
 
-        <input type="radio" id="qr4" name="reponse" value="qr4">
-        <label for="rep4">La balise src</label><br>
+            <p>Quel attribut permet d'indiquer la destination d'un lien ?</p>
 
-        <button type="submit">Valider</button>
-    </form>
-    </section>
+            <input type="radio" id="qr1" name="reponse4" value="qr13">
+            <label for="rep1"> la balise class</label><br>
 
-    <section>
-    <form action="" method="POST">
-        <p>Que permet l'attribut "tabindex" ?</p>
-    
-        <input type="radio" id="qr1" name="reponse" value="qr1">
-        <label for="rep1">De créer un index (plan) du site</label><br>
+            <input type="radio" id="qr2" name="reponse4" value="qr14">
+            <label for="rep2">la balise href</label><br>
 
-        <input type="radio" id="qr2" name="reponse" value="qr2">
-        <label for="rep2">De créer le parcours naturel de tabulation</label><br>
+            <input type="radio" id="qr3" name="reponse4" value="qr15">
+            <label for="rep3">La balise data</label><br>
 
-        <input type="radio" id="qr3" name="reponse" value="qr3">
-        <label for="rep3">De modifier le parcours naturel de tabulation</label><br>
+            <input type="radio" id="qr4" name="reponse4" value="qr16">
+            <label for="rep4">La balise src</label><br>
 
-        <input type="radio" id="qr4" name="reponse" value="qr4">
-        <label for="rep4">De forcer la naviguation uniquement avec son index </label><br>
 
-        <button type="submit">Valider</button>
-    </form>
+            <p>Que permet l'attribut "tabindex" ?</p>
+
+            <input type="radio" id="qr1" name="reponse5" value="qr17">
+            <label for="rep1">De créer un index (plan) du site</label><br>
+
+            <input type="radio" id="qr2" name="reponse5" value="qr18">
+            <label for="rep2">De créer le parcours naturel de tabulation</label><br>
+
+            <input type="radio" id="qr3" name="reponse5" value="qr19">
+            <label for="rep3">De modifier le parcours naturel de tabulation</label><br>
+
+            <input type="radio" id="qr4" name="reponse5" value="qr20">
+            <label for="rep4">De forcer la naviguation uniquement avec son index </label><br>
+
+            <button type="submit">Valider</button>
+        </form>
     </section>
 </body>
